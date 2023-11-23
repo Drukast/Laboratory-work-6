54f60c032de404b1fd861731a3f4ed783d1f4f7a
Ярцев О.Ю <132137309+Drukast@users.noreply.github.com>
2023-11-23 17:21:50 +0700

    form funtion finished

--- task2.js
+++ task2.js
@@ -1,4 +1,12 @@
 /*Для любого задания лабораторыных работ №5 и №6 реализовать ввод, формирование/обработку и вывод массивов с применением функций*/
 function form_matrix(N, M) {
-
+    matrix = []
+    for (var i = 0; i < M; i++) {
+        let submatrix = [];
+        for (var j = 0; j < N; j++) {
+            submatrix.push(Math.round((Math.random() * 100)));
+        }
+        matrix.push(submatrix);
+    }
+    return matrix
 }
