86b837786712407656b1712e1a72e9275fd6b910
Ярцев О.Ю <132137309+Drukast@users.noreply.github.com>
2023-11-24 19:32:16 +0700

    res added

--- task1.js
+++ task1.js
@@ -10,12 +10,13 @@ function search_max(a, b, c) {
     return max;
 }
 
-let a, b, c;
+let a, b, c, res;
 a = Number(prompt('Введите число a:', ''));
 b = Number(prompt('Введите число b:', ''));
 c = Number(prompt('Введите число c:', ''));
 if (!(isNaN(a)) && !(isNaN(b)) && !(isNaN(c))) {
-    alert(`${search_max(a, b, c)}`);
+    res = search_max(a, b, c)
+    alert(res);
 } else {
     alert('Введены нечисловые значения!');
 }
